11f9e737dc36260ad31eea1c1d9eb86490448c7a
Michael Bestas <mkbestas@gmail.com>
2024-12-29 04:35:19 +0200

    Switch to hardware/calyx/interfaces/power-libperfmgr
    Change-Id: Iac476194c9dd4f7b917702e861316afcc54276f0

--- common.mk
+++ common.mk
@@ -341,7 +341,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/calyx/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/samsung
 
